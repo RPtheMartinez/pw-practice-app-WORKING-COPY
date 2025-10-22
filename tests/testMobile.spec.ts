71e9d880d50f211c6dfc4ec839bc55879a93a48b
RPtheMartinez <kern_n@hotmail.com>
2025-10-22 13:08:40 -0700

    Add files via upload

--- tests/testMobile.spec.ts
+++ tests/testMobile.spec.ts
@@ -1,9 +1,21 @@
-import {test, expect} from ‘@playwright/test’
+import { test, expect } from '@playwright/test'
 
-test('Input Fields', async ({page}) => {
-	await page.goto('/')
-	await page.getByText('Forms').click()
-	await page.getByTest('Form Layouts').click()
+test('Input Fields', async ({ page }, testInfo) => {
+  await page.goto('/')
+
+  if (testInfo.project.name === 'mobile') {
+    // Close the sidebar for mobile viewports
+    await page.locator('.sidebar-toggle').click()
+  }
+
+  await page.locator('.sidebar-toggle').click()
+  await page.getByText('Forms').click()
+  await page.getByText('Form Layouts').click()
+
+  if (testInfo.project.name === 'mobile') {
+    // Close the sidebar for mobile viewports
+    await page.locator('.sidebar-toggle').click()
+  }
   
         const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
 
@@ -18,23 +30,4 @@ test('Input Fields', async ({page}) => {
 
         //Locator Assertions
         await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
-            });
-        
-            //Radio Buttons
-            test('Radio Buttons', async ({page}) => {
-                const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
-                
-                //Getting by the radio button label
-                //await usingTheGridForm.getByLabel('Option 1').check({force: true})
-                
-                //Getting by the role and name
-                await usingTheGridForm.getByRole('radio', {name: "Option 1"}).check({force: true})
-
-                const radioStatus = await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()
-                expect(radioStatus).toBeTruthy()
-
-                await expect(usingTheGridForm.getByRole('radio', {name: "Option 1"})).toBeChecked()
-                
-                await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true})
-                expect(await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked())
-            })
+      })
